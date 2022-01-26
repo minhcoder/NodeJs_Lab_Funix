ea61dc96b95a156153c96866aec98790405a9b2c
Nguyen Tuan Minh <minhnguyentuan31103@gmail.com>
2022-01-26 20:10:24 +0700

    LAB1.5

--- first-app.js
+++ first-app.js
@@ -1,14 +1,22 @@
 const http=require('http')
+const fs =require('fs')
 
 const server = http.createServer(function rqListener(req,res){
 console.log(req.url, req.method,req.headers)
 const url = (req.url)
+const method=(req.method)
 if(url==='/'){
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write('<body><form action="/message" method="POST"><input type="text" name="massage"/><button type="submit">Send</button></form></body>');
     res.write("</html>");
     return res.end();
+}
+if(url==='/message'&&method==='POST'){
+fs.writeFileSync('message.txt','DUMMY')
+res.statusCode=302;
+res.setHeader('Location','/')
+return res.end();
 }
 // process.exit();
 res.setHeader('Content-Type','text/html')
@@ -19,3 +27,6 @@ res.write("</html>");
 res.end();
 })
 server.listen(3000)
+
+
+
