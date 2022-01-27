58a6a5e8f9b2ba7cffc7b67d2650f1d80baa16a6
Nguyen Tuan Minh <minhnguyentuan31103@gmail.com>
2022-01-27 15:56:07 +0700

    LAB1.6

--- first-app.js
+++ first-app.js
@@ -1,8 +1,8 @@
 const http=require('http')
 const fs =require('fs')
+const { buffer } = require('stream/consumers')
 
 const server = http.createServer(function rqListener(req,res){
-console.log(req.url, req.method,req.headers)
 const url = (req.url)
 const method=(req.method)
 if(url==='/'){
@@ -12,19 +12,23 @@ if(url==='/'){
     res.write("</html>");
     return res.end();
 }
-if(url==='/message'&&method==='POST'){
-fs.writeFileSync('message.txt','DUMMY')
-res.statusCode=302;
-res.setHeader('Location','/')
-return res.end();
+if(url==='/message'&& method==='POST'){
+    const body=[]
+    req.on('data',(chunk)=>{
+        console.log(chunk)
+        body.push(chunk)
+    })
+    req.on('end',()=>{
+        const parsedBody=Buffer.concat(body).toString();
+        console.log(parsedBody)
+        const message= parsedBody.split('=')[1];
+        fs.writeFileSync('message.txt',message)
+    })
+    fs.writeFileSync('message.txt','DUMMY')
+    res.statusCode=302;
+    res.setHeader('Loacation','/')
+    return res.end();
 }
-// process.exit();
-res.setHeader('Content-Type','text/html')
-res.write('<html>');
-res.write('<head><title>This is My First Page</title></head>')
-res.write('<body><h1>Hello from my Node.js Server</h1></body>')
-res.write("</html>");
-res.end();
 })
 server.listen(3000)
 
