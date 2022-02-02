e1c9a45a95f60e1ace53a9dfb819bcaf8a003729
Nguyen Tuan Minh <minhnguyentuan31103@gmail.com>
2022-02-02 23:32:24 +0700

    LAB2.12

--- first-app.js
+++ first-app.js
@@ -9,6 +9,9 @@ app.use(bodyParser.urlencoded())
 app.use(adminRouters);
 app.use(shopRouters)
 
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found<h1/>')
+})
 
 
 app.listen(3000)
