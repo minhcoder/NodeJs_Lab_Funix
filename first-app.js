92d52b5418f74431ceac898552ffd79f83c7794a
Nguyen Tuan Minh <minhnguyentuan31103@gmail.com>
2022-02-02 20:28:27 +0700

    LAB2.5

--- first-app.js
+++ first-app.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app = express();
+const bodyParser= require('body-parser')
 
-// luôn luôn chạy
-app.use('/',(req,res,next)=>{
-    console.log('This Middleware always run')
-    next()
-})
+app.use(bodyParser.urlencoded())
 
 app.use('/add-product',(req,res,next)=>{
-    res.send('<h1>The "Add Product" Page</h1>')
+    res.send('<form action="/product" method="POST"><input type="text" name ="title"/><button type="submit">Add Product</button></form>')
 })
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+} )
 app.use('/',(req,res,next)=>{
     res.send('<h1>Hello from NodeJs</h1>')
 })
