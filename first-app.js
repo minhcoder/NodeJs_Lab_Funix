5d526b88875805964c21c3e9366e97746634056f
Nguyen Tuan Minh <minhnguyentuan31103@gmail.com>
2022-02-03 16:51:41 +0700

    LAB2.13

--- first-app.js
+++ first-app.js
@@ -1,17 +1,18 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require('path')
 const app = express();
-const bodyParser= require('body-parser')
-const adminRouters= require('./routes/admin')
-const shopRouters= require('./routes/shop')
 
-app.use(bodyParser.urlencoded())
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(adminRouters);
-app.use(shopRouters)
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found<h1/>')
-})
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
-app.listen(3000)
+app.listen(3000);
