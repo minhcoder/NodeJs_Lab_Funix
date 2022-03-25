d9f891074b4b42ba1876296ea78ea6d1ddee24cc
Nguyen Tuan Minh <minhnguyentuan31103@gmail.com>
2022-03-25 11:01:45 +0700

    LAB4.12

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -13,7 +14,11 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    res.render('shop/product-detail',{product: product,pageTitle: product.title, path: 'products'});
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
   });
 };
 
@@ -36,10 +41,11 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  console.log(prodId);
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
   res.redirect('/cart');
-
-}
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
