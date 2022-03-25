cf842be645cc9825dd38e7bfd21744d23b75ee5e
Nguyen Tuan Minh <minhnguyentuan31103@gmail.com>
2022-03-25 16:41:31 +0700

    LAB4.16

--- models/product.js
+++ models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -28,10 +29,19 @@ module.exports = class Product {
   save() {
     this.id = Math.random().toString();
     getProductsFromFile(products => {
+      if (this.id) {
+         const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+          const updatedProducts = [...products];
+          updatedProducts[existingProductIndex] = this;
+          fs.writeFile(p, JSON.stringify(updateProducts), err => {
+            console.log(err);
+          });
+      } else {
+        this.id = Math.random().toString();
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
-      });
+      });}
     });
   }
 
